0a48fc9a09349e62144b8e3766bfb8fc353f8de4
Ryan Li <ryanli@Ryans-MacBook-Pro.local>
2018-10-04 15:58:01 -1000

    Current files

---
+++ quiz2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	unsigned int a = 27;
+	a = a << 2;
+	printf("%x %d\n",a,a);
+	return 0;
+}
---
+++ quiz3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int a =  (1 << 3);
+	int b = (-1 >> 27);
+		printf("%d a, %d b\n",a,b);	
+	return 0;
+}
+
